c57f5aa322603ff1f5b33f2129fa9eacfe467714
Tejas Ladani <77571495+tejasladani999@users.noreply.github.com>
2023-10-23 17:08:54 +0530

    create UI for Profile page

--- client/src/components/Layout/Profile.js
+++ client/src/components/Layout/Profile.js
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div style={{maxWidth:"550px",margin:"0px auto"}}>
+        <div style={{ display:'flex', justifyContent:'space-around',margin:"18px 0px",borderBottom: "1px solid grey"}}> 
+            <div>
+                <img src="https://images.unsplash.com/photo-1503249023995-51b0f3778ccf?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D" alt="profilepic" style={{width:"160px",height:"160px",borderRadius:"80px"}} />
+            </div>
+            <div>
+                <h4>Tejas Ladani</h4>
+                <div style={{display: "flex",justifyContent:"space-between",width: '108%'}}>
+                    <h6>40 Posts</h6>
+                    <h6>40 followers</h6>
+                    <h6>40 following</h6>
+                </div>
+            </div>
+        </div>
+        <div className="userpost">
+            <img className='item' src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?auto=format&fit=crop&q=60&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8MXwwfHx8MA%3D%3D" alt="" />
+            <img className='item' src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?auto=format&fit=crop&q=60&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8MXwwfHx8MA%3D%3D" alt="" />
+            <img className='item' src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?auto=format&fit=crop&q=60&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8MXwwfHx8MA%3D%3D" alt="" />
+            <img className='item' src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?auto=format&fit=crop&q=60&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8MXwwfHx8MA%3D%3D" alt="" />
+            <img className='item' src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?auto=format&fit=crop&q=60&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8MXwwfHx8MA%3D%3D" alt="" />
+            <img className='item' src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?auto=format&fit=crop&q=60&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8MXwwfHx8MA%3D%3D" alt="" />
+        </div>
+    </div>
   )
 }
 
