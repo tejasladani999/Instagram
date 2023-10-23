8fa217b5f57f5c80c72052470536408af82c0c54
Tejas Ladani <77571495+tejasladani999@users.noreply.github.com>
2023-10-23 17:29:11 +0530

    create UI for Home page

--- client/src/components/Layout/Home.js
+++ client/src/components/Layout/Home.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+        <div className="card home-card">
+            <h5>Tejas Ladani</h5>
+            <div className="card-image">
+                <img src="https://images.unsplash.com/photo-1558981285-6f0c94958bb6?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHdhbGxwYXBlciUyMGJpa2V8ZW58MHwwfDB8fHww" alt="" />
+            </div>
+            <div className="card-content">
+            <i class="material-icons" style={{color:'red'}}>favorite</i>
+                <h6>title</h6>
+                <p>this is amazing Bike</p>
+                <input type="text" placeholder='add a comment'/>
+            </div>
+        </div>
+    </div>
   )
 }
 
