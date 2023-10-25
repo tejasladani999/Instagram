6c340f9114ccbe1cb230c963edccd512ef429a38
Tejas Ladani <77571495+tejasladani999@users.noreply.github.com>
2023-10-25 23:35:35 +0530

    Implement the Logout functionality

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React,{useContext} from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+import { UserContext } from '../App'
+
 const Navbar = () => {
+  const navigate= useNavigate()
+  const {state,dispatch} = useContext(UserContext)
+  const renderList = ()=>{
+    if(state){
+      return[
+        <li><Link to="/profile">Profile</Link></li>,
+        <li><Link to="/create">Create Post</Link></li>,
+        <li>
+        <button className='btn red lighten-1' onClick={()=>{
+        localStorage.clear()
+        dispatch({type:"CLEAR"})
+        navigate('/signin');
+        }}>Logout</button>
+        </li>
+      ]
+    }
+    else{
+      return[
+        <li><Link to="/signin">Login</Link></li>,
+        <li><Link to="/signup">Signup</Link></li>
+      ]
+    }
+  }
   return (
     <nav>
     <div className="nav-wrapper grey darken-4">
-      <Link to="/" className="brand-logo left">Instagram</Link>
+      <Link to={state?"/":"/signin"} className="brand-logo left">Instagram</Link>
       <ul id="nav-mobile" className="right">
-        <li><Link to="/signin">Login</Link></li>
-        <li><Link to="/signup">Signup</Link></li>
-        <li><Link to="/profile">Profile</Link></li>
-        <li><Link to="/create">Create Post</Link></li>
+        {renderList()}
       </ul>
     </div>
   </nav>
